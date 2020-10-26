178a8e695e8dbd12ab37d70f730f396a89a9853c
robcrockPI <robert@propellerindustries.com>
2020-10-26 06:23:42 -0300

    Updated the state and style of the button onClick.

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render () {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
@@ -8,7 +24,16 @@ export default class Popular extends React.Component {
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>{language}</button>
+            <button 
+              className='btn-clear nav-link'
+              style={language === this.state.selectedLanguage ? { color: '#dd0f0f'} : null}
+              // It's very important that we pass React a function definition and NOT
+              // a function invocation. If we were to only pass this.updateLanguage(language)
+              // React would immediatly invoke the function when it renders the button.
+              // To send a declaration we pass the button an arrow function.
+              onClick={() => this.updateLanguage(language)}>
+              {language}
+            </button>
           </li>
         ))}
       </ul>
