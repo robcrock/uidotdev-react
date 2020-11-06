763bf5c921bc1e2ba9bc8c2587578864c095308b
robcrockPI <robert@propellerindustries.com>
2020-11-06 10:00:18 -0300

    Users can now reset input boxes.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
 function Instructions () {
@@ -84,12 +84,98 @@ PlayerInput.propTypes = {
   label: PropTypes.string.isRequired
 }
 
+function PlayerPreview( {username, onReset, label}) {
+  return (
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className='row bg-light'>
+        <div className='player-info'>
+          <img
+            className='avatar-small'
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+        />
+        <a
+          href={`https://github.com/${username}`}
+          className='link'>
+          {username}
+        </a>
+        </div>
+      <button className='btn-clear flex-center' onClick={onReset}>
+        <FaTimesCircle color='rgb(194, 57, 42)' size={26} />
+      </button>
+      </div>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  // label: PropTypes.string.isRequred
+}
+
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    })
+  }
+
+  handleReset(id) {
+    this.setState({
+      [id]: null
+    })
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state;
+
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput label="Label!" onSubmit={ (value) => console.log(value) } />
+
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            { playerOne === null 
+              ? <PlayerInput
+                  label='Player One'
+                  onSubmit={ (player) => this.handleSubmit('playerOne', player) }
+                />
+              : <PlayerPreview
+                  username={playerOne}
+                  labael='Player One'
+                  onReset={ () => this.handleReset('playerOne') }
+                />
+            }
+
+
+            { playerTwo === null
+              ? <PlayerInput
+                  label='Player One'
+                  onSubmit={ (player) => this.handleSubmit('playerTwo', player) }
+                />
+              : <PlayerPreview
+                  username={playerTwo}
+                  labael='Player Two'
+                  onReset={ () => this.handleReset('playerTwo') }
+                />
+            }
+          </div>
+        </div>
       </React.Fragment>
     )
   }
